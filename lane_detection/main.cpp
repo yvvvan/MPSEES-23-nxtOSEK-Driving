537cfc3164c686b62b8209237c0cc8878f792b64
Christian Helbig <ch@christianhelbig.com>
2023-05-16 20:57:10 +0200

    Added OpenCV support in CMakeLists file, removed all errors, restructured code

--- lane_detection/main.cpp
+++ lane_detection/main.cpp
@@ -1,53 +1,78 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 
-Mat image_preprocessing(Mat frame) {
-    /**
-     * Does the following preprocessing steps:
-     *      - grayscaling
-     *      - masking
-     *      - gaussian blurring
-     *      - canny algorithm
-     *      - (binary image improvement - optional)
-     */
+
+/**
+ * image_preprocessing
+ *
+ * Does the following preprocessing steps:
+ *      - grayscaling
+ *      - masking
+ *      - gaussian blurring
+ *      - canny algorithm
+ *      - (binary image improvement - optional)
+ *
+ * @param frame
+ * @return
+ */
+cv::Mat image_preprocessing(cv::Mat frame) {
 
     // TODO FABIAN
 
-    return NULL;
+    cv::Mat placeholder;
+    return placeholder;
 }
 
-vector<Vec4i> line_filtering(vector<Vec4i> hough_lines) {
-    /**
-     * Find the right and left line on the preprocessed image. Remove unnecessary lines.
-     */
+/**
+ * line_filtering
+ *
+ * Find the right and left line on the preprocessed image. Remove unnecessary lines.
+ *
+ * @param hough_lines
+ * @return
+ */
+std::vector<cv::Vec4i> line_filtering(std::vector<cv::Vec4i> hough_lines) {
 
     // TODO CHRIS
 
-    return NULL;
+    std::vector<cv::Vec4i> placeholder;
+    return placeholder;
 }
 
-vector<Vec4i> calculate_center(vector<Vec4i> lane_lines) {
-    /**
-     * Find the center between the right and left line.
-     */
+/**
+ * calculate_center
+ *
+ * Find the center between the right and left line.
+ *
+ * @param lane_lines
+ * @return
+ */
+std::vector<cv::Vec4i> calculate_center(std::vector<cv::Vec4i> lane_lines) {
 
     // TODO FABIAN
 
-    return NULL;
+    std::vector<cv::Vec4i> placeholder;
+    return placeholder;
 }
 
+/**
+ * return_function
+ *
+ * This function communicates with other components. We are not sure what this function will return yet.
+ *
+ * @return
+ */
 int return_function() {
-    /**
-     * This function communicates with other components. We are not sure what this function will return yet.
-     */
 
     return 0;
 }
 
+/**
+ * main_loop
+ *
+ * This main loop will call all necessary functions for processing the image frames from the camera sensor.
+ */
 void main_loop() {
-    /**
-     * This main loop will call all necessary functions for processing the image frames from the camera sensor.
-     */
 
     // TODO CHRIS
 }
