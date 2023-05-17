b3543eeeb76209cbe5f2d6b7a605e9d6f3effa39
Christian Helbig <ch@christianhelbig.com>
2023-05-17 09:45:58 +0200

    Started implementation of main video loop

--- lane_detection/main.cpp
+++ lane_detection/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string.h>
 #include <opencv2/opencv.hpp>
 
 
@@ -68,18 +69,90 @@ int return_function() {
 }
 
 /**
- * main_loop
+ * main_loop_camera
  *
  * This main loop will call all necessary functions for processing the image frames from the camera sensor.
  */
-void main_loop() {
+void main_loop_camera() {
 
     // TODO CHRIS
 }
 
-int main() {
-    // Call main image processing loop
-    main_loop();
+/**
+ * main_loop_video
+ *
+ * This main loop will call all necessary functions for processing the image frames from a video.
+ *
+ * @param video_path Path to the video file
+ */
+void main_loop_video(const std::string& video_path) {
+    // open video file
+    cv::VideoCapture cap(video_path);
+    if (!cap.isOpened()) {
+        std::cout << "Error opening video stream or file" << std::endl;
+        return;
+    }
+
+    // creating an opencv window to display the processed frames
+    cv::namedWindow("Processed Frame", cv::WINDOW_NORMAL);
+
+    while (true) {
+        // read frame
+        cv::Mat frame;
+        cap >> frame;
+
+        // if frame is empty, video is over
+        if (frame.empty()) {
+            break;
+        }
+
+        // call image preprocessing
+        cv::Mat preprocessed_frame = image_preprocessing(frame);
+
+        // filter lines
+        std::vector<cv::Vec4i> hough_lines = line_filtering(preprocessed_frame);
+
+        // calculate center
+        std::vector<cv::Vec4i> lane_lines = calculate_center(hough_lines);
+
+        // send result to other components using the return_function function
+        // TODO complete the return_function function
+        return_function();
+
+        // draw lan_lines on frame
+        for (int i = 0; i < lane_lines.size(); i++) {
+            cv::Vec4i l = lane_lines[i];
+            cv::line(frame, cv::Point(l[0], l[1]), cv::Point(l[2], l[3]), cv::Scalar(0, 255, 0), 3, cv::LINE_AA);
+        }
+
+        // display processed frame
+        cv::imshow("Processed Frame", preprocessed_frame);
+
+        // exit if ESC is pressed
+        if (cv::waitKey(1) == 27) {
+            break;
+        }
+    }
+}
+
+int main(int argc, char *argv[]) {
+    // parse command line arguments
+    //      -image for a path to an image (will skip main loop)
+    //      -video for a path to a video
+    //      no argument for normal operation with camera sensor
+    if (strcmp(argv[1], "-image") == 0) {
+        // single image will be analyzed, skip main loop
+        // TODO
+    } else if (strcmp( argv[1], "-video") == 0) {
+        // video frames will be analyzed
+        main_loop_video(argv[2]);
+    } else if (argc < 2) {
+        // normal operation with camera sensor
+        main_loop_camera();
+    } else {
+        std::cout << "Please provide a valid argument." << std::endl;
+        return 1;
+    }
 
     return 0;
 }
