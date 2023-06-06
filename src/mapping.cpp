b089048bfd2a5d8a26854c6beb1a8645e7302bdc
Jakob Arndt <jakob.arndt@campus.tu-berlin.de>
2023-06-06 17:35:33 +0200

    src/mapping.src: Bugfixes
    Signed-off-by: Jakob Arndt <jakob.arndt@campus.tu-berlin.de>

--- src/mapping.cpp
+++ src/mapping.cpp
@@ -2,60 +2,78 @@
  * This file should contain the mapping class for the SESE driving projekt.
  * Currently it only contains a test script to test ORB_SLAM on the raspberry pi.
 */
-#include "core/libcamera_app.hpp"
-#include "core/options.hpp"
+#include <signal.h>
+#include <unistd.h>
+#include <opencv2/opencv.hpp>
+#include <ctime>
 
 #include <ORB_SLAM3/include/System.h>
 
-
-static void event_loop(LibcameraApp &app, ORB_SLAM3::System &slam)
-{
-    Options const *options = app.GetOptions();
-
-    app.OpenCamera();
-    app.ConfigureViewfinder();
-    app.StartCamera();
-
-    auto start_time = std::chrono::high_resolution_clock::now();
-
-    for (unsigned int count = 0; ; count++)
-    {
-        LibcameraApp::Msg msg = app.Wait();
-        if (msg.type == LibcameraApp::MsgType::Timeout)
-        {
-            LOG_ERROR("ERROR: Device timeout detected, attempting a restart!!!");
-            app.StopCamera();
-            app.StartCamera();
-            continue;
-        }
-        if (msg.type == LibcameraApp::MsgType::Quit)
-            return;
-        else if (msg.type != LibcameraApp::MsgType::RequestComplete)
-            throw std::runtime_error("unrecognised message!");
-
-        LOG(2, "Viewfinder frame " << count);
-        auto now = std::chrono::high_resolution_clock::now();
-        if (options->timeout && now - start_time > std::chrono::milliseconds(options->timeout))
-            return;
-
-        CompletedRequestPtr &completed_request = std::get<CompletedRequestPtr>(msg.payload);
-
-        slam.TrackMonocular(completed_request);
-    }
-
-    slam.Shutdown();
-}
+bool stop = false;
 
 int main(int argc, char **argv) {
 
+    /* handle keyboard interrupt */
+    struct sigaction sigIntHandler;
+    sigIntHandler.sa_handler = [](int s) {
+        std::cout << "Caught SIGINT, exiting..." << std::endl;
+        stop = true;
+    };
+
+    sigemptyset(&sigIntHandler.sa_mask);
+    sigIntHandler.sa_flags = 0;
+    sigaction(SIGINT, &sigIntHandler, nullptr);
+
     /* create slam instance */
-    const std::string vocabularyFile = "../libs/ORB_SLAM/Vocabulary/ORBvoc.txt";
-    const std::string configFile = "mono_raspi_cam.yaml";
-    const int frames = 10;
-    ORB_SLAM3::System slam(vocabularyFile, configFile, ORB_SLAM3::System::MONOCULAR, false);
+    const std::string vocabularyFile = "/home/pi/mpsees-23-nxtosek-driving/libs/ORB_SLAM3/Vocabulary/ORBvoc.txt";
+    const std::string configFile = "/home/pi/mpsees-23-nxtosek-driving/src/mono_raspi_cam.yaml";
+    ORB_SLAM3::System slam(vocabularyFile, configFile, ORB_SLAM3::System::MONOCULAR, false); // true to show visuals
 
-    /* create camera node */
-    LibcameraApp app;
+    std::string connstr = "libcamerasrc ! video/x-raw,framerate=20/1 ! appsink";
+    cv::VideoCapture cap(connstr, cv::CAP_GSTREAMER);
+    if (!cap.isOpened()) {
+        cerr << "ERROR! Unable to open camera\n";
+        return -1;
+    }
 
-    event_loop(app, slam);
+    while(!stop) {
+        cv::Mat frame;
+        cap.read(frame);
+
+        // If the frame was read successfully
+        if (!frame.empty()) {
+
+            // Get the current system time as a timestamp
+            std::time_t timestamp = std::time(nullptr);
+
+            Sophus::SE3f camera_pose = slam.TrackMonocular(frame, (double) timestamp);
+
+            Eigen::Quaternionf q = camera_pose.unit_quaternion();
+            Eigen::Vector3f twb = camera_pose.translation();
+            cout << twb(0) << " " << twb(1) << " " << twb(2) << " " << q.x() << " " << q.y() << " " << q.z() << " " << q.w() << endl;
+
+            /* Show current frame with opencv */
+            cv::imshow("Frame", frame);
+
+            // Wait for the 'q' key to be pressed to exit the loop
+            if (cv::waitKey(1) == 'q')
+                break;
+        } else {
+            cerr << "No frame received" << endl;
+            // If the frame was not read successfully, exit the loop
+            break;
+        }
+    }
+
+    /* Stop ORB SLAM */
+    if (slam.GetTrackingState() != ORB_SLAM3::Tracking::eTrackingState::SYSTEM_NOT_READY) {
+        slam.Shutdown();
+    }
+
+    /* Release the VideoCapture object and close OpenCV windows */
+    if (cap.isOpened()){
+        cap.release();
+    }
+    cv::destroyAllWindows();
+    exit(0);
 }
