dcbb95ff690f367762085d50dd7532294ee9b9e1
Fabian Riepel <fabian.riepel@campus.tu-berlin.de>
2023-06-11 13:22:56 +0200

    refined the line_filtering, added horizontal line detection, added check_intersection, added functions for the visualization to test

--- lane_detection/main.cpp
+++ lane_detection/main.cpp
@@ -49,13 +49,19 @@ cv::Mat image_preprocessing(cv::Mat frame) {
  * @param hough_lines
  * @return
  */
-void line_filtering(cv::Mat preprocessed_frame, cv::Vec4i &leftLane, cv::Vec4i &rightLane, cv::Vec4i &horizontalLine, cv::Vec4i &verticalLine) {
+void line_filtering(cv::Mat preprocessed_frame,
+                    cv::Vec4i &leftLane,
+                    cv::Vec4i &rightLane,
+                    std::vector<cv::Vec4i> &horizontalLine) {
   // perform probabilistic hough transform
   std::vector<cv::Vec4i> lines;
-  cv::HoughLinesP(preprocessed_frame, lines, 1, CV_PI / 180, 50, 30, 10);
+  cv::HoughLinesP(preprocessed_frame, lines, 1, CV_PI / 180, 50, 55, 5);
 
-  // check for horizontal lines
-  // TODO
+  /*cv::Mat houghImage = cv::Mat::zeros(preprocessed_frame.size(), CV_8UC3);
+  for (const cv::Vec4i &line : lines) {
+    cv::line(houghImage, cv::Point(line[0], line[1]), cv::Point(line[2], line[3]), cv::Scalar(0, 255, 0), 1);
+  }
+  cv::imshow("Hough", houghImage);*/ //just for visualization
 
   // separate left and right lane lines based on their slope
   std::vector<cv::Vec4i> leftLanes, rightLanes;
@@ -65,9 +71,10 @@ void line_filtering(cv::Mat preprocessed_frame, cv::Vec4i &leftLane, cv::Vec4i &
     // ignore lines with a small slope degree
     if (std::abs(slope) > 0.5) {
       // separate left and right lane lines
-      if (slope < 0)
+      //we need to check for both (slope and middle of the image) because if not we would detect a centerline outside the lane while turning in an intersection
+      if (slope < 0 && line[2] < preprocessed_frame.cols / 2)
         leftLanes.push_back(line);
-      else
+      else if (slope > 0 && line[2] > preprocessed_frame.cols / 2)
         rightLanes.push_back(line);
     }
   }
@@ -107,7 +114,17 @@ void line_filtering(cv::Mat preprocessed_frame, cv::Vec4i &leftLane, cv::Vec4i &
     y1 /= rightLanes.size();
     x2 /= rightLanes.size();
     y2 /= rightLanes.size();
-    rightLane = cv::Vec4i(x1, y1, x2, y2);
+    rightLane = cv::Vec4i(x2, y2, x1, y1); //coordinates the way around, so that is matches the normal coordinate system
+  }
+
+  horizontalLine.clear();
+  // check for horizontal lines
+  for (const cv::Vec4i &line : lines) {
+    // calculate dx and dy
+    int dx = abs(line[0] - line[2]);
+    int dy = abs(line[1] - line[3]);
+    // ignore lines with a small slope degree
+    if (dx > 6 * dy) horizontalLine.push_back(line);
   }
 }
 
@@ -125,9 +142,80 @@ void line_filtering(cv::Mat preprocessed_frame, cv::Vec4i &leftLane, cv::Vec4i &
  * @param verticalLine
  * @return
  */
-bool check_intersection(cv::Vec4i leftLane, cv::Vec4i rightLane, cv::Vec4i horizontalLines, cv::Vec4i verticalLines) {
-  // TODO Fabian
-  return false;
+bool check_intersection(cv::Vec4i leftLane,
+                        cv::Vec4i rightLane,
+                        std::vector<cv::Vec4i> horizontalLines,
+                        cv::Mat orig_image /* orig_image just a placeholder for visualization of common point, might delete later */) {
+
+  // Calculate the x-coordinate of the center of the image
+  int imageCenterX =
+      orig_image.cols / 2; //TODO find correct one (this is just a placeholder) -> needs to be the exact for every frame
+  bool check_left = false;
+  bool check_right = false;
+
+  // Loop through each horizontal line
+  for (const auto &horizontalLine : horizontalLines) {
+    // y = m * x + b for the horizontal line
+    float mhorizontal =
+        static_cast<float>(horizontalLine[1] - horizontalLine[3]) / (horizontalLine[0] - horizontalLine[2]);
+    int bhorizontal = horizontalLine[1] - static_cast<int>(mhorizontal * horizontalLine[0]);
+
+    //std::cout << "y(horizontal) = " << mhorizontal << " * x + " << bhorizontal << std::endl; //just for testing
+
+    // checking for intersections on the left side
+    if (horizontalLine[0] < imageCenterX && horizontalLine[2] < imageCenterX) {
+
+      // Calculate the intersection point with the left vertical line
+      float mLeft = static_cast<float>(leftLane[1] - leftLane[3]) / (leftLane[0] - leftLane[2]); //m = (y2-y1)/(x2-x1)
+      int bLeft = leftLane[1] - static_cast<int>(mLeft * leftLane[0]); // b = y1 - (m * x1)
+      int intersectionX = -static_cast<int>((bhorizontal - bLeft) / (mhorizontal - mLeft));
+
+      //int intersectionY = static_cast<int>(mLeft * intersectionX) + bLeft; //just for visualization, maybe needed if returning a point
+      //std::cout << "y(left) = " << mLeft << " * x + " << bLeft << std::endl; //just for testing
+
+      // Check if the intersection point is within the left lane segment
+      if (intersectionX < imageCenterX && 0 <= intersectionX) {
+
+        //std::cout << "X_left:" << intersectionX << " Y_left:" << intersectionY << std::endl; //just for testing
+        //cv::circle(orig_image, cv::Point(intersectionX, intersectionY), 3, cv::Scalar(255, 0, 0), 3); //just for testing
+
+        check_left = true;
+      }
+    }
+
+    // checking for intersections on the right side
+    if (horizontalLine[0] > imageCenterX && horizontalLine[2] > imageCenterX) {
+
+      // Calculate the intersection point with the right vertical line
+      float mRight = static_cast<float>(rightLane[1] - rightLane[3]) / (rightLane[0] - rightLane[2]);
+      int bRight = rightLane[1] - static_cast<int>(mRight * rightLane[0]);
+      int intersectionX = -static_cast<int>((bhorizontal - bRight) / (mhorizontal - mRight));
+
+      //int intersectionY = static_cast<int>(mRight * intersectionX) + bRight; //just for visualization, maybe needed if returning a point
+      //std::cout << "y(right) = " << mRight << " * x + " << bRight << std::endl;
+
+      // Check if the intersection point is within the right lane segment
+      if (intersectionX > imageCenterX && intersectionX <= 2 * imageCenterX) {
+
+        //std::cout << "X_right:" << intersectionX << " Y_right:" << intersectionY << std::endl; //just for testing
+        //cv::circle(orig_image, cv::Point(intersectionX, intersectionY), 3, cv::Scalar(255, 0, 0), 3); //just for testing
+
+        check_right = true;
+      }
+    }
+  }
+  // TODO calculate the average common point
+  if (check_left && check_right) {
+    std::cout << "Intersection on both sides detected" << std::endl;
+    return true;
+  } else if (check_left) {
+    std::cout << "Intersection on the left detected" << std::endl;
+    return true;
+  } else if (check_right) {
+    std::cout << "Intersection on the right detected" << std::endl;
+    return true;
+  } else
+    return false;
 }
 
 /**
@@ -146,19 +234,21 @@ std::vector<cv::Vec4i> calculate_center(cv::Vec4i &leftLane, cv::Vec4i &rightLan
 
     // Extract start points of the left and right lines
     cv::Point2f left_start(leftLane[0], leftLane[1]);
-    cv::Point2f right_start(rightLane[2], rightLane[3]);
+    cv::Point2f right_start(rightLane[0], rightLane[1]);
     double mid_start_x = (left_start.x + right_start.x) * 0.5;
     double mid_start_y = (left_start.y + right_start.y) * 0.5;
 
     // Extract end points of the left and right lines
     cv::Point2f left_end(leftLane[2], leftLane[3]);
-    cv::Point2f right_end(rightLane[0], rightLane[1]);
+    cv::Point2f right_end(rightLane[2], rightLane[3]);
     double mid_end_x = (left_end.x + right_end.x) * 0.5;
     double mid_end_y = (left_end.y + right_end.y) * 0.5;
 
     //create center between both lines
     cv::Vec4i center(mid_start_x, mid_start_y, mid_end_x, mid_end_y);
     center_line.push_back(center);
+
+    std::cout << "Centerline calculated." << std::endl;
 
   } else if (leftLane != cv::Vec4i()) {
 
@@ -179,6 +269,15 @@ std::vector<cv::Vec4i> calculate_center(cv::Vec4i &leftLane, cv::Vec4i &rightLan
 
   return center_line;
 }
+
+/*void drawLaneLine(cv::Mat &original_image, const cv::Vec4i &laneLine, const cv::Scalar &color) {
+  cv::line(original_image, cv::Point(laneLine[0], laneLine[1]), cv::Point(laneLine[2], laneLine[3]), color, 2);
+}
+void drawLaneLines(cv::Mat &original_image, std::vector<cv::Vec4i> &laneLines, const cv::Scalar &color) {
+  for (const cv::Vec4i &line : laneLines) {
+    cv::line(original_image, cv::Point(line[0], line[1]), cv::Point(line[2], line[3]), color, 2);
+  }
+} */ // just for drawing lines on the image, just needed for the visualization part in the "process_image"-function
 
 /**
  * return_function
@@ -207,28 +306,31 @@ void process_image_frame(cv::Mat frame) {
   // filter lines
   cv::Vec4i leftLane;
   cv::Vec4i rightLane;
-  cv::Vec4i horizontal_lines;
-  cv::Vec4i vertical_lines;
-  line_filtering(preprocessed_frame, leftLane, rightLane, horizontal_lines, vertical_lines);
+  std::vector<cv::Vec4i> horizontal_lines;
+  line_filtering(preprocessed_frame, leftLane, rightLane, horizontal_lines);
 
   // check if intersection is detected
-  bool is_intersection = check_intersection(leftLane, rightLane, horizontal_lines, vertical_lines);
+  // TODO complete intersection detection
+  bool is_intersection = check_intersection(leftLane, rightLane, horizontal_lines, frame);
 
   // calculate center
   std::vector<cv::Vec4i> lane_lines = calculate_center(leftLane, rightLane);
+
+  // Draw lane lines on the result image
+  /*cv::Mat result = frame.clone();
+  cv::Vec4i actual_center(result.cols / 2, 0, result.cols / 2, result.rows);
+  drawLaneLines(result, lane_lines, cv::Scalar(0, 0, 255));
+  drawLaneLine(result, actual_center, cv::Scalar(255, 0, 0));
+  drawLaneLine(result, leftLane, cv::Scalar(0, 255, 255)); //gelb
+  drawLaneLine(result, rightLane, cv::Scalar(255, 255, 0)); //türkis
+  drawLaneLines(result, horizontal_lines, cv::Scalar(0, 0, 0)); //schwarz
+  cv::imshow("Result with Centerline and actual center of the image", result);
+  cv::imshow("Processed Frame", preprocessed_frame);
+  cv::waitKey(0);*/ //just for visualization
+
   // send result to other components using the return_function function
   // TODO complete the return_function function
   return_function();
-
-  // draw lan_lines on frame
-  for (int i = 0; i < lane_lines.size(); i++) {
-    cv::Vec4i l = lane_lines[i];
-    cv::line(frame, cv::Point(l[0], l[1]), cv::Point(l[2], l[3]), cv::Scalar(0, 255, 0), 3, cv::LINE_AA);
-  }
-
-  // display processed frame
-  cv::imshow("Processed Frame", preprocessed_frame);
-  cv::waitKey(0);
 }
 
 /**
@@ -255,9 +357,6 @@ void main_loop_video(const std::string &video_path) {
     std::cout << "Error opening video stream or file" << std::endl;
     return;
   }
-
-  // creating an opencv window to display the processed frames
-  cv::namedWindow("Processed Frame", cv::WINDOW_NORMAL);
 
   while (true) {
     // read frame
@@ -294,8 +393,6 @@ int main(int argc, char *argv[]) {
   if (strcmp(argv[1], "-image") == 0) {
     // single image will be analyzed, skip main loop
     cv::Mat frame = cv::imread(argv[2]);
-    // creating an opencv window to display the processed frames
-    cv::namedWindow("Processed Frame", cv::WINDOW_NORMAL);
     // process image frame
     process_image_frame(frame);
   } else if (strcmp(argv[1], "-video") == 0) {
