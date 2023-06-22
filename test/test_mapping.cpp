686766d3d43caa0160f63c089044e9b5b28f8092
Jakob Arndt <jakob.arndt.de@googlemail.com>
2023-06-22 18:19:09 +0200

    test/test_mapping.cpp: Wrote Test for Mapping Class
    This commit adds a test for the test, class as well as some bug fixes
    detected while running the test.
    
    Signed-off-by: Jakob Arndt <jakob.arndt.de@googlemail.com>

---
+++ test/test_mapping.cpp
@@ -0,0 +1,70 @@
+#include <gtest/gtest.h>
+#include <fstream>
+#include <thread>
+
+
+#include "slam/mapping.hpp"
+
+BlackBoard &m_blackboard = BlackBoard::getInstance();
+
+std::vector<std::string> splitStringBySpace(const std::string& input) {
+    std::vector<std::string> tokens;
+    std::istringstream iss(input);
+    std::string token;
+
+    while (iss >> token) {
+        tokens.push_back(token);
+    }
+
+    return tokens;
+}
+
+/**
+ * @brief Create a fake ORB-SLAM thread which creates the coordinates
+ */
+int runFakeORBSLAM(const std::string& trajectory_file_name) {
+
+    /* open trajectory file */
+    auto trajectory_file = std::ifstream(trajectory_file_name);
+    if (!trajectory_file.is_open()) {
+        std::cout << "Error opening trajectory file" << std::endl;
+        return -1;
+    }
+
+    std::string line;
+    while (m_blackboard.localization_enabled.get() && std::getline(trajectory_file, line)) {
+        //std::cout << "line: " << line << std::endl;
+        auto values = splitStringBySpace(line);
+
+        Coordinates coordinates = Coordinates(stod(values[0]), stod(values[1]), 0);
+        m_blackboard.coordinates.set(coordinates);
+
+        m_blackboard.intersection_detected = strcasecmp("true",values[2].c_str()) == 0;
+
+        std::array<bool, 3> exits_detected {strcasecmp("true",values[3].c_str()) == 0,
+                                            strcasecmp("true",values[4].c_str()) == 0,
+                                            strcasecmp("true",values[5].c_str()) == 0};
+        m_blackboard.exits_detected.set(exits_detected);
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(200));
+    }
+    return 0;
+}
+
+TEST(MappingTest, MappingThreadTest) {
+
+    m_blackboard.localization_enabled.set(true);
+    m_blackboard.mapping_enabled.set(true);
+
+    /* start fake ORB-SLAM thread */
+    std::string trajectory_file_name = "/home/jakob/Documents/SESE_Projekt/mpsees/test/testTrajectory.txt";
+    std::thread fake_orb_slam_thread(runFakeORBSLAM, trajectory_file_name);
+
+    /* start mapping thread */
+    Mapping mapping = Mapping();
+    std::thread mapping_thread(&Mapping::exec_thread, &mapping);
+
+    /* wait for threads to finish */
+    fake_orb_slam_thread.join();
+    mapping_thread.join();
+}
