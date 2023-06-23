3f1c9ad079a319defa2617024d858066c04fce58
Yufan <yufan.dong@campus.tu-berlin.de>
2023-06-23 19:08:56 +0200

    1. add blackboard flag for mapping_finished, stop mapping when mapping_finished 2. add stations in blackboard and mapping

--- src/slam/mapping.cpp
+++ src/slam/mapping.cpp
@@ -24,7 +24,7 @@ int get_exit_number(std::tuple<double, double>current_exit){
             exit = exit_t::SOUTH_EXIT;
         }
     }
-    std::cout << current_exit_x << " " << current_exit_y << " "<< exit  << std::endl;
+    //std::cout << current_exit_x << " " << current_exit_y << " "<< exit  << std::endl;
     return exit;
 }
 
@@ -40,6 +40,7 @@ int get_exit_number(std::tuple<double, double>current_exit){
 */
 int get_next_direction(std::array<int, 4> exits, int current_exit) {
     int direction = direction_t::STRAIGHT;
+    // get the most left exit
     for (int i = 1; i < 4; i += 1) {
         if (exits.at((current_exit + i) % 4) == 0) {
             direction = i;
@@ -55,18 +56,19 @@ int get_next_direction(std::array<int, 4> exits, int current_exit) {
 
     // 1:left; 2:straight;3:right
     std::string text_direction = "";
-    if (direction == 1) {
-        text_direction = "left";
+    switch(direction) {
+        case 1:
+            text_direction = "left";
+            break;
+        case 2:
+            text_direction = "straight";
+            break;
+        case 3:
+        default:
+            text_direction = "right";
     }
-    if (direction == 2) {
-        text_direction = "straight";
-    }
-    if (direction == 3) {
-        text_direction = "right";
-    }
-    std::cout << current_exit << " go to " << text_direction << std::endl;
+    std::cout << "cuurent exit #" << current_exit << " go to " << text_direction << std::endl;
     return direction;
-
 }
 
 Mapping::Mapping() {
@@ -88,28 +90,60 @@ int Mapping::exec_thread() {
     double y_last = -1;
     bool intersection_detected = false;
     bool intersection_detected_last;
-    bool exit_left;
-    bool exit_right;
-    bool exit_middle;
+    bool exit_left_detected;
+    bool exit_right_detected;
+    bool exit_middle_detected;
     direction_t direction = direction_t::UNKNOWN;
-    bool is_finished = true;
+    station_t station = station_t::STATION_UNKNOWN;
+
+    this->blackboard.mapping_finished.set(false);
+    this->blackboard.stations.set(std::array<Coordinates,4>{Coordinates{0,0,0},Coordinates{0,0,0},Coordinates{0,0,0},Coordinates{0,0,0}});
+    std::cout << "Mapping started ..." << std::endl;
 
     while (this->blackboard.mapping_enabled.get()) {
 
+        // get the current position
+        x_last = x;
+        y_last = y;
         Coordinates coordinates = this->blackboard.coordinates.get();
         x = coordinates.x;
         y = coordinates.y;
-
         if (x == x_last && y == y_last)
             continue;
+        std::cout << "----- current: " << x << " " << y << " last: " << x_last << " " << y_last << std::endl;
 
-        std::cout << "x: " << x << " y: " << y << "x_last: " << x_last << " y_last: " << y_last << std::endl;
+        // get the station
+        station = this->blackboard.station.get();
+        if (station != station_t::STATION_UNKNOWN){
+            std::array<Coordinates,4> station_coordinates = this->blackboard.stations.get();
+            switch (station) {
+                case station_t::STATION_RED:
+                    station_coordinates.at(station_t::STATION_RED) = coordinates;
+                    break;
+                case station_t::STATION_GREEN:
+                    station_coordinates.at(station_t::STATION_GREEN) = coordinates;
+                    break;
+                case station_t::STATION_BLUE:
+                    station_coordinates.at(station_t::STATION_BLUE) = coordinates;
+                    break;
+                case station_t::STATION_YELLOW:
+                    station_coordinates.at(station_t::STATION_YELLOW) = coordinates;
+                    break;
+                case station_t::STATION_UNKNOWN:
+                default:
+                    break;
+            }   
+            // update the station coordinates
+            this->blackboard.stations.set(station_coordinates);
+        }
 
+
+        // get the intersection
         intersection_detected_last = intersection_detected;
         intersection_detected = this->blackboard.intersection_detected.get();
-        exit_left = this->blackboard.exits_detected.get().at(0);
-        exit_middle = this->blackboard.exits_detected.get().at(1);
-        exit_right = this->blackboard.exits_detected.get().at(2);
+        exit_left_detected = this->blackboard.exits_detected.get().at(0);
+        exit_middle_detected = this->blackboard.exits_detected.get().at(1);
+        exit_right_detected = this->blackboard.exits_detected.get().at(2);
 
         /* on a rising edge of intersection detected */
         if (intersection_detected && !intersection_detected_last) {
@@ -118,7 +152,7 @@ int Mapping::exec_thread() {
             // current_exit: the direction of entering an intersection
             std::tuple<double, double> current_exit (x-x_last, y-y_last);
 
-            // down:0;left:1;up:2;right:3
+            // south:0; west:1; north:2; east:3
             auto current_exit_number = (exit_t) get_exit_number(current_exit);
 
             /* initialise exit information arrays */
@@ -148,23 +182,20 @@ int Mapping::exec_thread() {
                 // get the next exit
                 direction = (direction_t) get_next_direction(exits_visit_stat, current_exit_number);
                 // leaving direction +1
-                exits_visit_stat.at((current_exit_number + (int) direction)%4) += 1;
+                exits_visit_stat.at((current_exit_number+direction)%4) += 1;
                 //update
                 map_intersection.find(current_intersection)->second = exits_visit_stat;
             }
 
-                // if new intersection
+            // if new intersection
             else {
                 // mark not-exits at intersection
-                if (!exit_left) {
+                if (!exit_left_detected) 
                     exits_visit_stat.at((current_exit_number+1)%4) = -1;
-                }
-                if (!exit_middle){
+                if (!exit_middle_detected)
                     exits_visit_stat.at((current_exit_number+2)%4) = -1;
-                }
-                if (!exit_right){
+                if (!exit_right_detected)
                     exits_visit_stat.at((current_exit_number+3)%4) = -1;
-                }
 
                 // comming direction +1
                 exits_visit_stat.at((current_exit_number)%4) += 1;
@@ -175,12 +206,14 @@ int Mapping::exec_thread() {
                 //update
                 map_intersection.insert(std::pair<std::tuple<double, double>,std::array<int, 4>>(current_position,exits_visit_stat));
             }
+
+            // update direction to blackboard
+            this->blackboard.direction.set(direction);
         }
 
 
-        // update direction to blackboard
-        this->blackboard.direction.set(direction);
 
+        bool is_finished = true;
         // check if all intersection finished
         for(const auto& elem : map_intersection){
             double intersection_x = std::get<0>(elem.first);
@@ -192,27 +225,26 @@ int Mapping::exec_thread() {
             }
         }
 
-        x_last = x;
-        y_last = y;
-
         if (is_finished && !map_intersection.empty()){
+            this->blackboard.mapping_finished.set(true);
             std::cout << "Mapping finished." << std::endl;
             break;
         }
 
     }
 
+    std::cout << "-----" << std::endl;
+    std::cout << "intersection result:" << std::endl;
+    std::cout << "intersection x,y | exit visit times S-W-N-E" << std::endl;
     for(const auto& elem : map_intersection){
         double intersection_x = std::get<0>(elem.first);
         double intersection_y = std::get<1>(elem.first);
-        int a0 =  std::get<0>(elem.second);
-        int a1 =  std::get<1>(elem.second);
-        int a2 =  std::get<2>(elem.second);
-        int a3 =  std::get<3>(elem.second);
-        std::cout << intersection_x  << " " <<  intersection_y << " " <<  a0 << " " <<  a1 << " " <<  a2 << " " <<  a3 << std::endl;
+        int south_exit =  std::get<0>(elem.second);
+        int left_exit =  std::get<1>(elem.second);
+        int north_exit =  std::get<2>(elem.second);
+        int right_exit =  std::get<3>(elem.second);
+        std::cout << intersection_x  << " \t" <<  intersection_y << " \t| " <<  south_exit << " \t" <<  left_exit << " \t" <<  north_exit << " \t" <<  right_exit << std::endl;
     }
-
-
 
     return 0;
 }
