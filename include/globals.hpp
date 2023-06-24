65af5183a02bdd30651c3e45ffe8035a866acd38
Christian Helbig <ch@christianhelbig.com>
2023-06-24 14:08:48 +0200

    Added parameters of lane detection to globals variable

--- include/globals.hpp
+++ include/globals.hpp
@@ -41,4 +41,25 @@ constexpr char SERIAL_DEVICE[] = "/dev/serial0";
 constexpr double FORWARD_ANGLE = 120; // everything above this angle is considered in place turning
 constexpr double FORWARD_SCALE_FACTOR = (-1.0 / (FORWARD_ANGLE / 2.0));
 
+/* lane detection */
+constexpr int IMAGE_WIDTH  = 640;
+constexpr int IMAGE_HEIGHT = 480;
+constexpr int FPS          = 20;
+constexpr double IMAGE_MIDDLE = IMAGE_WIDTH / 2;
+
+// canny
+constexpr int CANNY_THRESHOLD_1 = 50;
+constexpr int CANNY_THRESHOLD_2 = 150;
+
+// hough
+constexpr int HOUGH_RHO         = 1;
+constexpr double HOUGH_THETA    = CV_PI / 180;
+constexpr int HOUGH_THRESHOLD   = 15;
+constexpr int HOUGH_MIN_LINELEN = 10;
+constexpr int HOUGH_MAX_LINEGAP = 20;
+
+// lane detection
+constexpr double LANE_DETECTION_MIN_SLOPE = 0.5;
+constexpr double MISSING_LANE_MULTIPLIER  = 0.12;
+
 #endif //BUILDHAT_GLOBALS_HPP
